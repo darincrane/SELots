89bb5526d01702266114de6a785f591df7a4c862
Gabriel Rivera <Toran3@WebToranPro1.local>
2012-01-13 17:20:58 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SELots::Application.config.secret_token = '4d406027e920312186347464992ebc87853a85d2b691092c0c3bd0fcc16d6ca6dc74e76d41024120fe64aec779903325d731c7a662f71f098adf5fa5372abf13'
